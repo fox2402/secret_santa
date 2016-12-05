a9e354e552aca037d4ab9a4c9456b09506340bd0
Kenan Lejosne <kenan.lejosne@epita.fr>
2016-12-05 17:41:13 +0100

    [RELEASE] Version 1.0

--- secret_santa.py
+++ secret_santa.py
@@ -1,38 +1,46 @@
-#!/bin/ython3
+#!/bin/python3
 
 
 import os
 import pdb
 import argparse
+import random
 
 class santa:
     def __init__(self, inp):
-        gver = []
-        rver = []
         with open(inp, "r") as f:
+            self.gver = []
+            self.rver = []
             for line in f:
-                gver.append(line)
-                rver.append(line)
+                self.gver.append(line[0:len(line) - 1])
+                self.rver.append(line[0:len(line) - 1])
 
-    def print_list():
+    def print_list(self):
         i = 0
-        for elt in gver:
+        for elt in self.gver:
             print (i, elt)
             i += 1
-    def run():
+    def run(self):
         while (True):
             print ("Select your name: (number expected, -1 to quit)")
-            print_list()
+            self.print_list()
             c = int(input())
             if c == -1:
                 break
-            if c > len(gver) or c < 0:
+            if c >= len(self.gver) or c < 0:
                 print ("invalid input")
                 continue
-            i = random.randrage(0, len(rver))
-            print ("You'll give " + rver[i] + " a present!")
-            gver.remove(gver[c])
-            rver.remove(rver[i])
+            while (True):
+                i = random.randint(0, len(self.rver) - 1)
+                if (self.gver[c] != self.rver[i]):
+                    break
+
+            print ("You'll give " + self.rver[i] + " a present!")
+            self.gver.remove(self.gver[c])
+            self.rver.remove(self.rver[i])
+            print ("Press key to hide this result and keep it secret!")
+            g = input()
+            os.system('cls' if os.name == 'nt' else 'clear')
 
 
 
@@ -43,11 +51,14 @@ class santa:
 
 def main():
     parser = argparse.ArgumentParser(description = "Secret Santa!")
-    parser.add_argument("-i", "--input", type = str,
-            description = "Input name list, see README for the format")
+    parser.add_argument("-i", "--input", type = str)
     args = parser.parse_args()
     if args.input is None:
         print ("secret_sant: No input file give, run with -i or --input")
-        print ("** Are all the children in the universe that for you not
-        to consider anything for their Christmas?!**")
+        print ("** Are all the children in the universe that for you not\
+ to consider anything for their Christmas?!**")
         return
+    s = santa(args.input)
+    s.run()
+
+main()
